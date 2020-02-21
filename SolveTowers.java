64cd959fa8c752f0b91a3f6fb1cce2a1e02e1476
rjedwards105 <54290212+rjedwards105@users.noreply.github.com>
2020-02-21 00:25:52 -0800

    done
    done

---
+++ SolveTowers.java
@@ -0,0 +1,17 @@
+//*******************************************************
+//  SolverTowers.java       Author: Lewis/Loftus
+//
+//  Demonstrates recursion.
+//*******************************************************
+public class SolveTowers
+{
+    //------------------------------------------------------------------
+    //  Creates a TowersOfHanoi puzzle and solves it.
+    //------------------------------------------------------------------
+    public static void main (String[] args)
+    {
+        TowersOfHanoi towers = new TowersOfHanoi (4);
+
+        towers.solve();
+    }
+}
